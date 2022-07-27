7704a1380129b523f8473fd68874267e21d87bf1
Kenta Tanizawa <kt10025102@gmail.com>
2022-07-27 23:06:20 +0900

    enum

---
+++ src/enum.ts
@@ -0,0 +1,29 @@
+export {};
+
+enum Months {
+  January = 1,
+  February,
+  March,
+  April,
+  Jun,
+  July,
+  August,
+  September,
+  October,
+  November,
+  December,
+}
+
+Months.January;
+Months.February;
+
+enum COLORS {
+  RED = "#FF0000",
+  WHITE = "#FFFFFF",
+  GREEN = "#008000",
+  BLUE = "#0000FF",
+  BLACK = "#000000",
+}
+
+let green = COLORS.GREEN;
+console.log(green);
